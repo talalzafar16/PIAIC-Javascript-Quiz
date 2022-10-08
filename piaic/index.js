ab484033318d4ad00dfd2f10d9047f2006b62d2c
talal <talalzafar84@gmail.com>
2022-10-09 03:10:06 +0500

    pushing files

---
+++ piaic/index.js
@@ -0,0 +1,769 @@
+// // -------Exercise 2---------
+// let a="ali"
+// alert(`hello ${a}, would you like to learn some python today?`)
+
+
+
+
+// // -------Exercise 3---------
+// let a="taLAl zAfaR";
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
+// let b=a.toLowerCase().split(' ').map((e,i)=>{
+//         return e.charAt(0).toUpperCase() +e.slice(1) ;
+// })
+// console.log(b.join(' '))
+
+
+
+
+// // -------Exercise 4---------
+// console.log(`Shiekh Sadi once said, "Virtue is in the mind, not in the appearance." `)
+    
+
+
+
+
+// // -------Exercise 5 ---------
+// let famous_person="Sheikh Sadi";
+// let message=famous_person +' once said, "Virtue is in the mind, not in the appearance."'
+// console.log(message);
+
+
+
+
+// // -------Exercise 6---------
+// let x="wahid"
+// console.log("hi \t"+x+"\t how are you? \n\t where are you?")
+
+
+
+
+
+// // -------Exercise 7 & 8---------
+// console.log(6+2)
+// console.log(10-2)
+// console.log(4*2)
+// console.log(16/2)
+
+
+
+
+
+// // -------Exercise 9---------
+// let x=7
+// console.log("my favorite number is "+ x)
+
+
+
+
+// // -------Exercise 10---------
+// let x="talal"  //storing my name in letirable
+// console.log("my name is "+x)  //printing name
+
+
+
+
+
+// // -------Exercise 11---------
+// let arr=["talal","wahid","salman"]
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+
+
+
+
+// // -------Exercise 12---------
+// let arr=["talal","wahid","salman"]
+// let msg="hi!,"
+// console.log(msg+arr[0])
+// console.log(msg+arr[2])
+// console.log(msg+arr[1])
+
+
+
+
+// // -------Exercise 13---------
+// let arr=["honda motorbike","civic car","lamgorghini car"]
+// let stat="i would like to own "
+// console.log(stat+arr[0])
+// console.log(stat+arr[1])
+// console.log(stat+arr[2])
+
+
+
+
+
+
+// // -------Exercise 14---------
+// let arr=["ali","nehal","wahid"]
+// let msg =", you are invited to dinner!"
+// console.log(arr[0]+msg)
+// console.log(arr[1]+msg)
+// console.log(arr[2]+msg)
+
+
+
+
+// // -------Exercise 15---------
+// let arr=["ali","nehal","wahid"]
+// let msg =", you are invited to dinner!"
+// console.log(arr[0]+msg)
+// console.log(arr[1]+msg)
+// console.log(arr[2]+msg)
+// console.log(arr[0]+" cant come to dinner!")
+// arr[0]="ahsan"
+// console.log(arr[0]+msg)
+// console.log(arr[1]+msg)
+// console.log(arr[2]+msg)
+
+
+
+
+
+// // -------Exercise 16---------
+// let arr=["ali","nehal","wahid"]
+// let msg =", you are invited to dinner!"
+// console.log(arr[0]+msg)
+// console.log(arr[1]+msg)
+// console.log(arr[2]+msg)
+// console.log(arr[0]+" cant come to dinner!")
+// arr[0]="ahsan"
+// console.log(arr[0]+msg)
+// console.log(arr[1]+msg)
+// console.log(arr[2]+msg)
+// console.log("found a bigger table")
+// arr.splice(0,0,"salman")
+// arr.splice(2,0,"sharukh")
+// arr.push("shakil")
+// console.log(arr[0]+msg)
+// console.log(arr[1]+msg)
+// console.log(arr[2]+msg)
+// console.log(arr[3]+msg)
+// console.log(arr[4]+msg)
+// console.log(arr[5]+msg)
+
+
+
+// // // -------Exercise 17---------
+/*
+let arr=["ali","nehal","wahid"]
+let msg =", you are invited to dinner!"
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log(arr[2]+msg)
+console.log(arr[0]+" cant come to dinner!")
+arr[0]="ahsan"
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log(arr[2]+msg)
+console.log("found a bigger table")
+arr.splice(0,0,"salman")
+arr.splice(2,0,"sharukh")
+arr.push("shakil")
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log(arr[2]+msg)
+console.log(arr[3]+msg)
+console.log(arr[4]+msg)
+console.log(arr[5]+msg)
+console.log("sorry we can invite only two people!")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+*/
+
+
+
+
+
+// // -------Exercise 18---------
+/*
+arr=["france","norway","america","australlia","russia"]
+console.log(arr)   //orignal order
+console.log([...arr].sort())   //alphabetical order
+console.log(arr)   // again orignal order
+console.log([...arr].sort().reverse())   // reverse alphabetical order
+console.log(arr)   // again orignal order
+console.log(arr.reverse())   // reverse alphabetical order affecting orignal array
+console.log(arr.reverse())   // reverse alphabetical order making orignal array
+arr.sort()
+console.log(arr)   //alphabetical sort affecting orignal array
+arr.sort().reverse()
+console.log(arr)   // alphabetically reverse sort affecting orignal array
+*/
+
+
+
+
+
+// // -------Exercise 19---------
+//coping program 17
+/*
+let arr=["ali","nehal","wahid"]
+let msg =", you are invited to dinner!"
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log(arr[2]+msg)
+console.log(arr[0]+" cant come to dinner!")
+arr[0]="ahsan"
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log(arr[2]+msg)
+console.log("found a bigger table")
+arr.splice(0,0,"salman")
+arr.splice(2,0,"sharukh")
+arr.push("shakil")
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log(arr[2]+msg)
+console.log(arr[3]+msg)
+console.log(arr[4]+msg)
+console.log(arr[5]+msg)
+console.log("sorry we can invite only two people!")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+let a=arr.pop()
+console.log("sorry "+ a +" your invitation is cancelled")
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log("number of people i am inviting to dinner are "+arr.length)  //printing number of people inviting to dinner
+*/
+
+
+
+
+
+
+
+// // -------Exercise 20---------
+/*
+let arr=[]  
+arr[0]=["Sindh","Punjab","Balochistan"]
+arr[1]=["Indus","Ravi","Satluj"]
+arr[2]=["Urdu","English","Punjabi"]
+console.log(arr)  //array containing list of items
+*/
+
+
+
+
+
+// // -------Exercise 21---------
+/*
+let obj={}
+obj.id=231
+obj.name="ali"
+obj.age=22
+obj.profession="student"
+console.log(obj)
+*/
+
+
+
+
+
+// // -------Exercise 22---------
+//Intentional Error
+/*
+let arr=["ali","nehal","wahid"]
+let msg =", you are invited to dinner!"
+console.log(arr[0]+msg)
+console.log(arr[1]+msg)
+console.log(arr[28]+msg)    //index error because index is greater than array length
+*/
+
+
+
+
+// // -------Exercise 23---------
+//Conditional Test
+/*
+let car = 'honda';
+console.log("Is car == 'hoanda'? I predict True.")
+console.log(car == 'honda')
+
+let name = 'ali';
+console.log("Is name == 'ali'? I predict True.")
+console.log(name == 'ali')
+
+let age = 33;
+console.log("Is age ==33 ? I predict True.")
+console.log(age == 33)
+
+let x = 77;
+console.log("Is x == 77? I predict True.")
+console.log(x == 77)
+
+let city = 'karachi';
+console.log("Is city == 'karchi'? I predict True.")
+console.log(city == 'karachi')
+
+let province = 'sindh';
+console.log("Is province == 'KPK'? I predict False.")
+console.log(province == 'KPK')
+
+let brand = 'adidas';
+console.log("Is brand == 'nike'? I predict False.")
+console.log(brand == 'nike')
+
+let phone = 'infinix';
+console.log("Is phone == 'iphone'? I predict False.")
+console.log(phone == 'iphone')
+
+let number = 33;
+console.log("Is number ==23 ? I predict False.")
+console.log(number == 23)
+
+let grade = 12;
+console.log("Is grade == 8? I predict False.")
+console.log(grade == 8)
+*/
+
+
+
+
+
+// // -------Exercise 24---------
+
+/*
+// Tests for equality and inequality with strings
+name1="Ali"
+name2="shoiab"
+name3="shoiab"
+console.log(name2==name3);
+console.log(name2==name1);
+console.log(name2!=name1);
+console.log(name2!=name3);
+
+//Tests using the lower case function
+console.log(name1==name1.toLowerCase());
+console.log(name2==name2.toLowerCase());
+
+//Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+num1=3;
+num2=6
+num3=3
+console.log(num1==num3);
+console.log(num1!=num3);
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1<=num2);
+console.log(num1>=num2);
+
+//Tests using "and" and "or" operators
+console.log(num1==num3 && num1==num2);
+console.log(num1==num3 || num1==num2);
+// Test whether an item is in a array
+arr=[1,2,3,4]
+console.log(arr.includes(3))
+
+//Test whether an item is not in a array
+console.log(!(arr.includes(7)));
+
+*/
+
+
+
+
+
+
+
+
+// // -------Exercise 25---------
+/*
+let alien_color="green";
+let alien_color1="green";
+if(alien_color=="green"){
+    console.log("the player just earned 5 points.")   //passes the test
+}
+if(alien_color1=="green"){}     // fails the test
+*/
+
+
+
+
+
+// // -------Exercise 26---------
+/*
+//Run if block
+let alien_color="green";
+if(alien_color=="green"){
+    console.log("the player just earned 5 points for shooting the alien..")   
+}
+else{
+    console.log("the player just earned 10 points.")   
+}
+
+//Run else block
+let alien_color1="red";
+if(alien_color1=="green"){
+    console.log("the player just earned 5 points for shooting the alien..")   
+}
+else{
+    console.log("the player just earned 10 points.")   
+}
+*/
+
+
+
+
+// // -------Exercise 27---------
+/*
+//Run if block
+let alien_color="green";
+if(alien_color=="green"){
+    console.log("the player just earned 5 points.")   
+}
+else if(alien_color=="yellow"){
+    console.log("the player just earned 10 points.")   
+}
+else if(alien_color=="red"){
+    console.log("the player just earned 15 points.")   
+}
+
+//Run ifelse first block yellow one
+let alien_color1="yellow";
+if(alien_color1=="green"){
+    console.log("the player just earned 5 points.")   
+}
+else if(alien_color1=="yellow"){
+    console.log("the player just earned 10 points.")   
+}
+else if(alien_color1=="red"){
+    console.log("the player just earned 15 points.")   
+}
+
+//Run ifelse second block red one
+let alien_color2="red"
+if(alien_color2=="green"){
+    console.log("the player just earned 5 points.")   
+}
+else if(alien_color2=="yellow"){
+    console.log("the player just earned 10 points.")   
+}
+else if(alien_color2=="red"){
+    console.log("the player just earned 15 points.")   
+}
+*/
+
+
+
+
+
+
+// // -------Exercise 28---------
+//Stage of life 
+/*
+let age=44;
+if(age<2){console.log("the person is a baby.")}
+else if(age>=2 && age<4){console.log("the person is toddler")}
+else if(age>=4 && age<13){console.log("the person is kid")}
+else if(age>=13 && age<20){console.log("the person is teenager")}
+else if(age>=20 && age<65){console.log("the person is adult")}
+else if(age>=65){console.log("the person is elder")}
+else{console.log("wrong age")}
+*/
+
+
+
+
+
+// // -------Exercise 29---------
+//Favourite Fruit
+/*
+let favorite_fruits=["apple","mango",'banana']
+let a='banana';
+let b="appricot";
+let c="apple";
+let d="pear";
+let e="mango";
+if(favorite_fruits.includes(a)){console.log("You really like "+a)}
+if(favorite_fruits.includes(b)){console.log("You really like "+b)}
+if(favorite_fruits.includes(c)){console.log("You really like "+c)}
+if(favorite_fruits.includes(d)){console.log("You really like "+d)}
+if(favorite_fruits.includes(e)){console.log("You really like "+e)}
+*/
+
+
+
+
+
+
+// // -------Exercise 30---------
+/*
+users=["admin","eric","ali","talal","zardari"]
+for(let e=0;e<users.length;e++){
+    users[e]=="admin"?console.log(`Hello ${users[e]}, would you like to see a status report?`):console.log(`Hello ${users[e]}, thank you for logging in again.`)
+}
+*/
+
+
+
+
+// // -------Exercise 31---------
+//No users
+/*
+users=["ali"]
+users.length==0&&console.log(" We need to find some users!")
+users.pop()
+users.length==0&&console.log(" We need to find some users!")
+*/
+
+
+
+
+
+
+// // -------Exercise 32---------
+/*
+current_users=["ali","talal","osama","ashraf","zia"]
+new_users=["TALAL","nehal","ahsan","zia","musharaf"]
+for(let i=0;i<new_users.length;i++){
+    if(current_users.includes(new_users[i].toLowerCase())){console.log("the person "+new_users[i] +" will need to enter a new username")}
+    else{console.log(new_users[i]+" the username is available")}
+}
+*/
+
+
+
+
+
+
+// // -------Exercise 33---------
+/*
+//Ordinal Number
+arr=[1,2,3,4,5,6,7,8,9]
+for(let i=0; i<arr.length; i++){
+    if(arr[i]==1){console.log(arr[i]+"st")}
+    else if(arr[i]==2){console.log(arr[i]+"nd")}
+    else if(arr[i]==3){console.log(arr[i]+"rd")}
+    else{console.log(arr[i]+"th")}
+}
+*/
+
+
+
+
+
+
+// // -------Exercise 34---------
+/*
+pizza=["BBQ","Pepperoni","Afghani"]
+for(let i=0;i<pizza.length;i++){
+    console.log(`I like ${pizza[i]} Pizza`)
+}
+console.log("I really love pizza!")
+*/
+
+
+
+
+
+
+// // -------Exercise 35---------
+/*
+animals=["rabbit","panda","zebra"];
+for(let i=0;i<animals.length;i++){
+    console.log(`a ${animals[i]}  would make a great pet`)
+}
+console.log("these all animal eat grass");
+*/
+
+
+
+
+
+
+
+// // -------Exercise 36---------
+/*
+//T-shirt
+function make_shirt(size,text){
+    console.log("size of shirt is "+size+" and the messsage printed on shirt is "+text);
+}
+make_shirt("large","'i am awesome'")
+*/
+
+
+
+
+
+
+// // -------Exercise 37---------
+/*
+function make_shirt(size='large', message='I love javacsript!'){
+    console.log("I'm going to make a " + size + " t-shirt.")
+    console.log('It will say, "' + message + '"')}
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'i am awesome.')
+*/
+
+
+
+
+
+// // -------Exercise 38---------
+/*
+function describe_city(city,country="Pakistan"){
+    console.log(city+" is in "+country)
+}
+describe_city("karachi")
+describe_city("lahore")
+describe_city("moscow","Russia")
+*/
+
+
+
+
+
+// // -------Exercise 39---------
+/*
+function describe_city(city,country){
+    return(
+        `"${city},${country}"`
+        )
+}
+console.log(describe_city("karachi","Pakistan"))
+console.log(describe_city("islamabad","pakistan"))
+console.log(describe_city("london","Usa"))
+*/
+
+
+
+
+
+// // -------Exercise 40---------
+/*
+function make_album(artist_name,album_title,track=0){
+    let obj={"artist_name":artist_name, "album_title":album_title}
+    if(track!=0){
+        obj.track=track;
+    }
+    return obj;
+}
+let album=make_album("elle duhe","in the middle of nights")
+console.log(album)
+let album1=make_album("bebe rexha","in the name of love")
+console.log(album1)
+let album2=make_album("ed sheeran","perfect",3)
+console.log(album2)
+*/
+
+
+
+
+
+
+
+
+// // -------Exercise 41---------
+/*
+function show_magicians(arr){
+    arr.map((e)=>{console.log(e)})
+}
+magician_names=["nick","ezla","dayn","lucifer"]
+show_magicians(magician_names);
+*/
+
+
+
+
+
+
+// // -------Exercise 42---------
+/*
+function show_magicians(arr){
+    arr.map((e)=>{console.log(e)})
+}
+
+function make_great(arr){
+    let great=[]
+    arr.map((e,i)=>{
+        let name="the great "+e;
+        great.push(name)})
+    magician_names=[...great]
+}
+magician_names=["nick","ezla","dayn","lucifer"]
+show_magicians(magician_names);
+make_great(magician_names);
+show_magicians(magician_names);
+*/
+
+
+
+
+
+
+// // -------Exercise 43---------
+/*
+function show_magicians(arr){
+    arr.map((e)=>{console.log(e)})
+}
+
+function make_great(arr){
+    let great=[]
+    arr.map((e,i)=>{
+        let name="the great "+e;
+        great.push(name)})
+    return great
+}
+magician_names=["nick","ezla","dayn","lucifer"]
+show_magicians(magician_names);
+let new_array =make_great(magician_names)
+show_magicians(new_array);
+*/
+
+
+
+
+
+
+
+
+
+// // -------Exercise 44--------
+/*
+function sandwitch(...sand){
+    sand.map((e)=>{
+    console.log(`..your sandwitch includes ${e}`)})
+    console.log("enjjoy your sandwitch! \n\n")
+}
+sandwitch("ketchup","beef")
+sandwitch("ketchup","cheese","chicken")
+sandwitch("mayonese","beef","butter")
+*/
+
+
+
+
+
+
+
+// // -------Exercise 45--------
+/*
+function cars_info(manu,model,...other){
+    car_obj={"manufacturer":manu,"model":model}
+    other.map((e)=>{
+        car_obj[Object.keys(e)]=Object.values(e)[0]
+    })
+    console.log(car_obj)
+}
+cars_info("honda","civic",)
+cars_info("toyota","corolla",{price:22342})
+cars_info("honda","city",{headlight:"round"},{color:"black"})
+*/
